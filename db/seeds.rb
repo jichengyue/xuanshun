c189314e050c1d97b66eb57c73f1c2b607693ce8
jichengyue <ji19910119@163.com>
2016-05-09 08:30:27 +0800

    1、更改数据库为PostgreSQL 2、添加seeds数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email:"jichengyue@yuetech.com.cn",password:"yuetech1234")
